3e50a47cd0f3ce090ae88cbdd3a17eba2dbaeace
jnosek <jnosek@gmail.com>
2018-07-22 22:17:57 -0400

    TableView example showing mis-selection behavior

---
+++ TableView.SelectionTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TableView.SelectionTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            var textCell = (TextCell)sender;
+
+            textCell.Text = "I was CLICKED";
+        }
+    }
+}
