0670f6591efab5416c965868e5148d2db91f1700
jnosek <jnosek@gmail.com>
2018-08-15 18:00:37 -0400

    Added reset option to MainPage

--- TableView.SelectionTest/MainPage.xaml.cs
+++ TableView.SelectionTest/MainPage.xaml.cs
@@ -21,5 +21,26 @@ namespace TableView.SelectionTest
 
             textCell.Text = "I was CLICKED";
         }
+
+        void Reset_Tapped(object sender, System.EventArgs e)
+        {
+            var textCell = (TextCell)sender;
+            var table = (Xamarin.Forms.TableView)textCell.Parent;
+
+            foreach (var section in table.Root)
+            {
+                foreach (var cell in section)
+                {
+                    var tc = cell as TextCell;
+
+                    if (tc != null && 
+                        tc != textCell && 
+                        tc.Text != "Click Me")
+                    {
+                        tc.Text = "Click Me";
+                    }
+                }
+            }
+        }
     }
 }
