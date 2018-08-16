55cabd02c5c15d73a1cedb661ff5d9a73d02274e
jnosek <jnosek@gmail.com>
2018-08-16 16:27:21 -0400

    a fix that utilizes customer renders, until officially released

---
+++ TableView.SelectionTest.iOS/CustomTableViewModelRenderer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using XF = Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace TableView.SelectionTest.iOS
+{
+    public class CustomTableViewModelRenderer : TableViewModelRenderer
+    {
+        public CustomTableViewModelRenderer(XF.TableView model)
+            : base(model)
+        {
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            View.Model.RowSelected(indexPath.Section, indexPath.Row);
+            if (AutomaticallyDeselect)
+                tableView.DeselectRow(indexPath, true);
+
+            tableView.EndEditing(true);
+        }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            BindGestures(tableView);
+            return View.Model.GetSectionCount();
+        }
+
+        void BindGestures(UITableView tableview)
+        {
+            if (HasBoundGestures)
+                return;
+
+            HasBoundGestures = true;
+
+            var gesture = new UILongPressGestureRecognizer(LongPress);
+            gesture.MinimumPressDuration = 2;
+            tableview.AddGestureRecognizer(gesture);
+
+            var dismissGesture = new UITapGestureRecognizer(Tap);
+            dismissGesture.CancelsTouchesInView = false;
+            tableview.AddGestureRecognizer(dismissGesture);
+
+            Table = tableview;
+        }
+
+        void Tap(UITapGestureRecognizer gesture)
+        {
+            // if the gesture point is in the view and maps to a table index, do not end editing
+            // this forces the keyboard to close, adjusting the screen and changing the index on RowSelected
+            // the keyboard should close after RowSelected is complete
+
+            var point = gesture.LocationInView(Table);
+            var indexPath = Table.IndexPathForRowAtPoint(point);
+            if (indexPath != null)
+                return;
+
+            gesture.View.EndEditing(true);
+        }
+    }
+}
---
+++ TableView.SelectionTest.iOS/CustomTableViewRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XF = Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(XF.TableView), typeof(TableView.SelectionTest.iOS.CustomTableViewRenderer))]
+namespace TableView.SelectionTest.iOS
+{
+    public class CustomTableViewRenderer : TableViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<XF.TableView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                SetSource();
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == XF.TableView.HasUnevenRowsProperty.PropertyName)
+            {
+                SetSource();
+            }
+        }
+
+        void SetSource()
+        {
+            var modeledView = Element;
+            Control.Source = modeledView.HasUnevenRows ?
+                (UITableViewSource)new UnEvenTableViewModelRenderer(modeledView) :
+                (UITableViewSource)new CustomTableViewModelRenderer(modeledView);
+        }
+    }
+}
